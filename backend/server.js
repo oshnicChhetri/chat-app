af4b7875d53dd9f8c2c14fefa48cccb595e57b98
oshnicChhetri <oshnicchhetri75@gmail.com>
2024-08-03 20:31:44 +0200

    deployment

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,12 @@ import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import connectToMongoDb from "./db/connectDatabase.js";
 import messageRoutes from "./routes/message.route.js";
-
+import path from "path";
 import {app,server} from "./socket/socket.js"
 
 const PORT = process.env.PORT || 5000 ;
 
+const __dirname = path.resolve();
 
 dotenv.config();
 app.use(express.json()); // to parse the incoming requests with Json payload (from req.body)
@@ -29,6 +30,13 @@ app.use(cookieParser()); // middleware to acesss cookies
 app.use("/api/auth/", authRoutes);
 app.use("/api/messages/", messageRoutes);
 app.use("/api/users/", userRoutes);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*", (req,res) =>{
+    res.sendfile(path.join,(__dirname,"frontend","dist","index.html"))
+})
+
 
 server.listen(PORT, () =>{
  connectToMongoDb();
